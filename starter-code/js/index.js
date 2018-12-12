27925f268e8c7b8513e2780b580ea83ebc8be1f9
Detlev Brodowski <brodowskid@mediamarktsaturn.com>
2018-12-12 17:29:17 +0100

    Iteration 3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,66 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function updatePriceByProduct(product, productPrice) {
+  const sumDom = product.querySelector(".sum");
+  sumDom.innerText = productPrice.toFixed(2);
 }
 
 function getTotalPrice() {
+  let totalPrice = 0;
+  const productsDom = document.querySelectorAll(".products > article");
+  productsDom.forEach(product => {
+    console.log(product);
+    const singlePrice = parseFloat(product.querySelector(".price").innerText);
+    console.log(singlePrice);
+    const amount = parseInt(product.querySelector("input").value);
+    console.log(amount);
+    sum = amount * singlePrice;
+    updatePriceByProduct(product, sum)
+    totalPrice += sum;
+  })
+  const totalPriceDom = document.querySelector("#total-price");
+  totalPriceDom.innerText = totalPrice.toFixed(2);
+}
+
+function createQuantityInput() {
 
 }
 
-function createQuantityInput(){
+function createDeleteButton() {
 
 }
 
-function createDeleteButton(){
+function createQuantityNode() {
 
 }
 
-function createQuantityNode(){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createItemNode(dataType, itemData){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItem() {
 
 }
 
-function createNewItem(){
+window.onload = () => {
+  const calculatePriceButton = document.getElementById("calc-prices-button");
+  // let createItemButton = document.getElementById("new-item-create");
+  // let deleteButtons = document.getElementsByClassName("btn-delete");
 
-}
+  calculatePriceButton.addEventListener("click", getTotalPrice);
+  // createItemButton.onclick = createNewItem;
 
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  // for(var i = 0; i<deleteButtons.length ; i++) {
+  //   deleteButtons[i].onclick = deleteItem;
+  // }
 };
